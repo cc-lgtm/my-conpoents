d597300b3a62fea8d8a6384b025e8fb72abd37f2
coderC <2448856797@qq.com>
2021-07-13 15:54:13 +0800

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button/button';
+import { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={(e) => {e.preventDefault(); alert(123)}}> hello </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        hello
+      </Button>
+      <Button target="_blank" btnType={ButtonType.Link} href="http://www.baidu.com">
+        baidu link
+      </Button>
     </div>
   );
 }
